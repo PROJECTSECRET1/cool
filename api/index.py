3c1e0215ddd069aeffa07922cef5ce9917767e18
PROJECTSECRET1 <137121821+PROJECTSECRET1@users.noreply.github.com>
2025-03-28 17:00:00 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
@@ -7,16 +7,6 @@ VALID_KEYS = ["key1", "key2", "key3"]
 @app.route('/api/keys', methods=['GET'])
 def get_keys():
     return jsonify(VALID_KEYS)
-
-@app.route('/api/validate_key', methods=['POST'])
-def validate_key():
-    data = request.json
-    key = data.get('key')
-    
-    if key in VALID_KEYS:
-        return jsonify({"valid": True})
-    else:
-        return jsonify({"valid": False})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
